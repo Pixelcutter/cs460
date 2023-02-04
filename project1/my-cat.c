a2bfb40f9fbe7750fa5161e42f7e21c5939745bc
pixel_cutter <j.diamond@students.clark.edu>
2023-02-03 21:29:59 -0800

    cat made, need to add comments

--- project1/my-cat.c
+++ project1/my-cat.c
@@ -3,10 +3,32 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[])
 {
-    printf("hello world\n");
+    // no files specified on the command line
+    if(argc < 2)
+        return 0;
+    
+    int fd;
+    int buffSize = 56;
+    char buffer[buffSize];
+    int fileCount = 1;
+    
+    while(fileCount < argc){
+        if(fd = open(argv[fileCount], O_RDONLY), fd < 0){
+            printf("my-cat: cannot open file\n");
+            exit(1);
+        }
+
+        int amtRead = 0;
+        while(amtRead = read(fd, buffer, buffSize), amtRead > 0)
+            write(STDOUT_FILENO, buffer, amtRead);
+        
+        fileCount++;
+        close(fd);
+    }
    
     return 0;
 }
